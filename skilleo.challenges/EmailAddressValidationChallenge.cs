05c90e2143d605a2441aadaf427c734ad900a005
steind <davestein@hotmail.com>
2014-06-04 16:34:16 -0400

    creating new project

---
+++ skilleo.challenges/EmailAddressValidationChallenge.cs
@@ -0,0 +1,18 @@
+﻿namespace skilleo.challenges
+{
+    public class EmailAddressValidationChallenge
+    {
+        public static string ProcessInput(string input)
+        {
+            var result = "Valid";
+
+            if (input.Contains(" "))
+            {
+                // input must not contain spaces
+                result = "Invalid";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ skilleo.tests/EmailAddressValidation.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using skilleo.challenges;
+
+namespace skilleo.tests
+{
+    [TestClass]
+    public class EmailAddressValidation
+    {
+        private const string ValidString = "Valid";
+        private const string InvalidString = "Invalid";
+
+        [TestMethod]
+        public void SampleInput1_ShouldBeInvalid()
+        {
+            const string input = "random text";
+            var output = EmailAddressValidationChallenge.ProcessInput(input);
+
+            Assert.IsTrue(output == InvalidString);
+        }
+
+        [TestMethod]
+        public void SampleInput2_ShouldBeValid()
+        {
+            const string input = "some_name@email.com";
+            var output = EmailAddressValidationChallenge.ProcessInput(input);
+
+            Assert.IsTrue(output == ValidString);
+        }
+    }
+}
