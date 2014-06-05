36cd4c9701e2b0ddcf0a852706ddf755807057bd
dstein <davestein@hotmail.com>
2014-06-04 21:29:14 -0400

    adding Date Validator challenge

---
+++ skilleo.challenges/DateValidatorChallenge.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace skilleo.challenges
+{
+    public class DateValidatorChallenge
+    {
+        public static string ProcessInput(string input)
+        {
+            var dates = input.Split('-');
+            DateTime date1;
+            DateTime date2;
+
+            // input must contain 2 dates
+            if (dates.Length != 2)
+            {
+                return "Invalid";
+            }
+
+            // dates must be valid dates in the format mm/dd/yyyy.
+            if (!DateTime.TryParseExact(dates[0].Trim(), "MM/dd/yyyy", CultureInfo.CurrentCulture, DateTimeStyles.None, out date1))
+            {
+                return "Invalid";
+            }
+
+            if (!DateTime.TryParseExact(dates[1].Trim(), "MM/dd/yyyy", CultureInfo.CurrentCulture, DateTimeStyles.None, out date2))
+            {
+                return "Invalid";
+            }
+
+            // date1 must be before date2
+            return date1 < date2 ? "Valid" : "Invalid";
+        }
+    }
+}
---
+++ skilleo.tests/DateValidator.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using skilleo.challenges;
+
+namespace skilleo.tests
+{
+    [TestClass]
+    public class DateValidator
+    {
+        private const string ValidString = "Valid";
+        private const string InvalidString = "Invalid";
+
+        [TestMethod]
+        public void InputWithLessThanTwoDates_ShouldBeInvalid()
+        {
+            const string input = "12/23/2013";
+
+            var output = DateValidatorChallenge.ProcessInput(input);
+
+            Assert.IsTrue(output == InvalidString);
+        }
+
+        [TestMethod]
+        public void InputWithMoreThanTwoDates_ShouldBeInvalid()
+        {
+            const string input = "12/23/2013 - 10/01/2014 - 12/31/2014";
+
+            var output = DateValidatorChallenge.ProcessInput(input);
+
+            Assert.IsTrue(output == InvalidString);
+        }
+
+        [TestMethod]
+        public void InputWithFirsDateBeforeSecondDate_ShouldBeValid()
+        {
+            const string input = "12/23/2013 - 10/01/2014";
+
+            var output = DateValidatorChallenge.ProcessInput(input);
+
+            Assert.IsTrue(output == ValidString);
+        }
+
+        [TestMethod]
+        public void InputWithFirsDateEqualToSecondDate_ShouldBeInvalid()
+        {
+            const string input = "12/23/2013 - 12/23/2013";
+
+            var output = DateValidatorChallenge.ProcessInput(input);
+
+            Assert.IsTrue(output == InvalidString);
+        }
+
+        [TestMethod]
+        public void InputWithFirsDateAfterToSecondDate_ShouldBeInvalid()
+        {
+            const string input = "12/23/2013 - 12/23/2010";
+
+            var output = DateValidatorChallenge.ProcessInput(input);
+
+            Assert.IsTrue(output == InvalidString);
+        }
+
+        [TestMethod]
+        public void InputWithWrongFormattedDate_ShouldBeInvalid()
+        {
+            const string input = "11/22/2013 - 23/01/2015";
+
+            var output = DateValidatorChallenge.ProcessInput(input);
+
+            Assert.IsTrue(output == InvalidString);
+        }
+
+    }
+}
