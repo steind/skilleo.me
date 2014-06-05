6b608889bb79dd13bfe5bb8e1e6c822030f3d46d
dstein <davestein@hotmail.com>
2014-06-04 22:50:49 -0400

    adding The Brute Force challenge

---
+++ skilleo.challenges/TheBruteForceChallenge.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace skilleo.challenges
+{
+    public class TheBruteForceChallenge : Challenge
+    {
+        public override string ProcessInput(string input)
+        {
+            var allowedCharacters = new[] { 'a', 'b', 'c', '0', '1', '2', '3' };
+
+            var md5 = MD5.Create();
+
+            for (var a = 0; a < allowedCharacters.Count(); a++)
+            {
+                for (var b = 0; b < allowedCharacters.Count(); b++)
+                {
+                    for (var c = 0; c < allowedCharacters.Count(); c++)
+                    {
+                        for (var d = 0; d < allowedCharacters.Count(); d++)
+                        {
+                            // guess represents an array of bytes from allowedCharacters.
+                            var guessBytes = Encoding.UTF8.GetBytes(new[]
+                            {
+                                allowedCharacters[a], 
+                                allowedCharacters[b], 
+                                allowedCharacters[c], 
+                                allowedCharacters[d]
+                            });
+
+                            // Compute a hash for guess.
+                            var guessHash = md5.ComputeHash(guessBytes);
+
+                            // Convert the bytes of the hashed guess to a hex string.
+                            var guessHashHexString = new StringBuilder();
+                            foreach (var b1 in guessHash)
+                            {
+                                guessHashHexString.Append(b1.ToString("x2"));
+                            }
+
+                            // Does the hash value of the guess match the input hash?
+                            if (guessHashHexString.ToString() == input)
+                            {
+                                return Encoding.UTF8.GetString(guessBytes);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ skilleo.tests/TheBruteForce.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using skilleo.challenges;
+
+namespace skilleo.tests
+{
+    [TestClass]
+    public class TheBruteForce
+    {
+        [TestMethod]
+        public void SkilleoSampleInput_ShouldWorkAsExpected()
+        {
+            const string hash = "81b5dd04bf5cbc172eeb34bb8062fde1";
+
+            var output = new TheBruteForceChallenge().ProcessInput(hash);
+
+            Assert.AreEqual("a23c", output);
+        }
+    }
+}
